4f0bbba85bf93fee646e625010ff64d4399efb6d
maroua <marouabekkaoui@hotmail.com>
2024-01-04 10:44:49 +0100

    fix addition bug

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,10 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		System.out.println(Calculatrice.add(1, 5));
+		System.out.println(Calculatrice.add(2, 5));
+		System.out.println(Calculatrice.add(3, 5));
+		System.out.println(Calculatrice.add(4, 5));
+		System.out.println(Calculatrice.add(6, 5));
 	}
 }
