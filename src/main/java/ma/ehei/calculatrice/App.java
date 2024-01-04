b23ed00a335b582e16a6d8839730e8a3deb98336
x-speed-2 <annasisub100@gmail.com>
2024-01-04 10:36:24 +0100

    Multiply Function

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,6 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		System.out.println(Calculatrice.multiply(5, 5));
 	}
 }
